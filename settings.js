6f860bc57af07d613f3f5aee06ccb0ff42f7fe88
Christof Schwarz <christof.schwarz@gmx.at>
2020-10-01 16:10:41 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,7 @@
 define([], function() {
 	return {
-		editMode_mainDivStyle: "display:table; height:99%; background-image:linear-gradient(45deg,#c9f2df 5%,#ffffff 5%,#ffffff 50%,#c9f2df 50%,#c9f2df 55%,#ffffff 55%,#ffffff 100%); background-size:14.14px 14.14px;"
+		currSelectionsFormula: "=GetCurrentSelections(Chr(10),',',',',60)"
+		,editMode_mainDivStyle: "display:table; height:99%; background-image:linear-gradient(45deg,#c9f2df 5%,#ffffff 5%,#ffffff 50%,#c9f2df 50%,#c9f2df 55%,#ffffff 55%,#ffffff 100%); background-size:14.14px 14.14px;"
 		,editMode_innerSpanStyle: "display:table-cell; vertical-align:middle; text-align:center;"
 		
 		// Headline div-Container
@@ -12,6 +13,9 @@ define([], function() {
 		,singleMode_aTagRightStyle: "padding: 0px 10px 3px 10px; background-color: #efefef;"
 		,button_fontColorActive: '#111'
 		,button_fontColorInactive: '#bbb'
+		,popOver_outerStyle: 'position: absolute; top: 64px; width:400px; height: 300px;right: 36px; margin-right: 10px; margin-bottom: 10px;'
+		,popOver_innerStyle: 'padding: 1px 20px;'
+		,popOver_aStyle: 'text-decoration:none; color:#222;'
 		
 	};
 });        
